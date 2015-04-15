58c1003a6a3e4d1f1dcac87a4e51d5df77b8faa5
Chris Moreton <chris@netsensia.com>
2015-04-15 12:14:13 +0100

    Retries when cURL call fails

--- src/Sauce/Sausage/SauceAPI.php
+++ src/Sauce/Sausage/SauceAPI.php
@@ -31,48 +31,61 @@ class SauceAPI
 
     protected function makeRequest($url, $type="GET", $params=false)
     {
-        $ch = curl_init();
-        if($this->verify_certs == false) {
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $attempts = 0;
+        
+        do {
+            $ch = curl_init();
+            if($this->verify_certs == false) {
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            }
+    
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_USERPWD, $this->username.":".$this->access_key);
+    
+            if ($type == "POST")
+                curl_setopt($ch, CURLOPT_POST, 1);
+            elseif ($type == "PUT")
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            elseif ($type == "DELETE")
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+    
+            // If user has ca bundle location specified in environment
+            // set that same path on php curl connection
+            if (getenv('CURL_CA_BUNDLE')) {
+                curl_setopt($ch, CURLOPT_CAINFO, getenv('CURL_CA_BUNDLE'));
+            }
+    
+            $headers = array();
+            $headers[] = 'Content-Type: text/json';
+    
+            $data = '';
+            if ($params) {
+                $data = json_encode($params);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            }
+            $headers[] = 'Content-length:'.strlen($data);
+    
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    
+            $response = curl_exec($ch);
+    
+            $failed = false;
+            if (curl_errno($ch)) {
+                $failed = true;
+                $error = curl_error($ch);
+                curl_close($ch);
+            }
+            
+        } while ($failed && ++$attempts < 10);
+
+        if ($failed) {
+            throw new \Exception("Got an error while making a request (tried " . $attempts . " times): ".$error);
         }
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERPWD, $this->username.":".$this->access_key);
-
-        if ($type == "POST")
-            curl_setopt($ch, CURLOPT_POST, 1);
-        elseif ($type == "PUT")
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-        elseif ($type == "DELETE")
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-
-        // If user has ca bundle location specified in environment
-        // set that same path on php curl connection
-        if (getenv('CURL_CA_BUNDLE')) {
-            curl_setopt($ch, CURLOPT_CAINFO, getenv('CURL_CA_BUNDLE'));
-        }
-
-        $headers = array();
-        $headers[] = 'Content-Type: text/json';
-
-        $data = '';
-        if ($params) {
-            $data = json_encode($params);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        }
-        $headers[] = 'Content-length:'.strlen($data);
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        $response = curl_exec($ch);
-
-        if (curl_errno($ch))
-            throw new \Exception("Got an error while making a request: ".curl_error($ch));
-
+        
         curl_close($ch);
-
+        
         return $this->convertResult($response);
     }
 
